e7c1d6e574976ecb683041fbb04c7f3e05df2f4c
Essy <Essy-Gaming@Essy-HP.(none)>
2016-07-09 23:50:42 -0700

    Github isn't noticing my Accept Header.

--- goauth.go
+++ goauth.go
@@ -5,6 +5,7 @@
 package goauth
 import(
 	"net/url"
+	"io/ioutil"
 	"encoding/json"
 	"fmt"
 	"google.golang.org/appengine/urlfetch"	
@@ -27,9 +28,9 @@ var(
 	//////////////////////////////////////////////////////////////////////////////////
 	ErrCrossSite= errors.New("Error: Terminate Request, Cross Site Attack Detected")
 	//////////////////////////////////////////////////////////////////////////////////
-	// ErrNoData is given if 1 or More expected values but get 0.
+	// ErrNoData is given if 1 or more pieces of data were expected and 0 were recieved.
 	//////////////////////////////////////////////////////////////////////////////////
-	ErrNoData   = errors.New("Error: No Data")
+	ErrNoData = errors.New("Error: No Data Found")
 )
 
 type GoogleToken struct {
@@ -192,7 +193,6 @@ func googleRecieve(req *http.Request, redirect ,googleid, googlesecretid string,
 
 
 func requiredRecieve(req *http.Request, clientID, secretID, redirect, src string) (*http.Response, error) {
-	req.Header.Set("Accept","application/json")
 	ctx := appengine.NewContext(req)
 	values := make(url.Values)
 	_ , memErr := memcache.Get(ctx, req.FormValue("state"))
@@ -230,28 +230,45 @@ func dropboxRecieve(req *http.Request, redirect ,clientID, secretID string, toke
 	
 //////////////////////////////////////////////////////////////////////////////////
 // Recieve for Github OAuth
+/// 
 //////////////////////////////////////////////////////////////////////////////////	
-type githubData struct {
-	AccessToken string `json:"access_token"`
-	Scope		string `json:"scope"`
-	TokenType	string `json:"token_type"`
-}
 func githubRecieve(req *http.Request, redirect ,clientID, secretID string, token *GitHubToken) error {
-	res, err := requiredRecieve(req, clientID, secretID, redirect, "https://github.com/login/oauth/access_token")
-	if err != nil { return err }
-	var ghd githubData
-	err = extractValue(res,&ghd)
-	if err != nil { return err }	
+	ctx := appengine.NewContext(req)	
 	
-	ctx := appengine.NewContext(req)
+	_ , memErr := memcache.Get(ctx, req.FormValue("state"))
+	if memErr != nil { return ErrCrossSite }
+	
+	values := make(url.Values)
+	values.Add("client_id", clientID)
+	values.Add("client_secret", secretID)
+	values.Add("code", req.FormValue("code"))
+	values.Add("state", req.FormValue("redirect"))
+
 	client := urlfetch.Client(ctx)
-	res2, err := client.Get("https://api.github.com/user/emails?access_token=" + ghd.AccessToken)
+	response0, err := client.PostForm("https://github.com/login/oauth/access_token", values)
+	if err != nil {
+		return err
+	}
+	defer response0.Body.Close()
+
+	bs, err := ioutil.ReadAll(response0.Body)
+	if err != nil {
+		return err
+	}
+
+	values, err = url.ParseQuery(string(bs))
+	if err != nil { return err }
+	accessToken := values.Get("access_token")
+
+	client2 := urlfetch.Client(ctx)
+	response, err := client2.Get("https://api.github.com/user/emails?access_token=" + accessToken)
 	if err != nil { return err }
 
 	var data []GitHubToken
-	err = extractValue(res2,&data)
+	err = extractValue(response,&data)
+	if err != nil { return err }	
 	if len(data) == 0 { return ErrNoData }
-	
+
 	*token = data[0]
 	token.State = strings.Split(req.FormValue("state"),"](|)[")[1]
 	return nil
