9e928e63101a51e6ca7a98c67be8b6c60363f40e
Essy <Essy-Gaming@Essy-HP.(none)>
2016-08-10 11:15:20 -0700

    Changed Github structure

--- github.go
+++ github.go
@@ -1,15 +1,30 @@
 package goauth
 import(
 	"net/http"
+	"net/url"
 	"fmt"
 	"strings"
 )
 
 type GitHubToken struct {
+	AccessToken string `json:"access_token"`
+	Scope		string `json:"scope"`
+	TokenType	string `json:"token_type"`
+	State		string
+}
+
+func (d GitHubToken)Email(req *http.Request)(GitHubEmail , error){
+	ai := []GitHubEmail{}
+	values := make(url.Values)
+	values.Add("access_token",d.AccessToken)
+	err := CallAPI(req,"GET", "https://api.github.com/user/emails", values, &ai)	
+	return ai[0],err
+}
+	
+type GitHubEmail struct {
 	Email    string
 	Verified bool
 	Primary  bool
-	State 	 string
 }
 
 
@@ -29,31 +44,13 @@ func githubSend(res http.ResponseWriter, req *http.Request, redirect ,clientID s
 /// So Without the ability to unmarshal this ends up way uglier than it should 
 /// be.
 //////////////////////////////////////////////////////////////////////////////////	
-type githubData struct {
-	AccessToken string `json:"access_token"`
-	Scope		string `json:"scope"`
-	TokenType	string `json:"token_type"`
-}
 func githubRecieve(res http.ResponseWriter, req *http.Request, redirect ,clientID, secretID string, token *GitHubToken) error {
 	resp, err := requiredRecieve(res,req,clientID,secretID,redirect,"https://github.com/login/oauth/access_token") 
 	if err != nil { return err }
 
-	var ghd githubData
+	var ghd GitHubToken
 	err = extractValue(resp,&ghd)
 	if err != nil { return err }
-	
-	// Make second request.
-	response, err := internalClient(req).Get("https://api.github.com/user/emails?access_token=" + ghd.AccessToken)
-	if err != nil { return err }
-
-	// Extract token. Make sure there is data.
-	var data []GitHubToken
-	err = extractValue(response,&data)
-	if err != nil { return err }	
-	if len(data) == 0 { return ErrNoData }
-
-	// Attatch data and state.
-	*token = data[0]
 	token.State = strings.Split(req.FormValue("state"),"](|)[")[1]
 	return nil
 }
